92bcbecc86785ec0b230c2e2db39b226d4aef26c
Herostrates94 <p.birgiel@gmail.com>
2018-03-31 13:14:22 +0200

    Adding a simple unit test

---
+++ src/test/java/com/banking/bank/BankApplicationTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.banking.bank;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pbirg
+ */
+public class BankApplicationTest {
+    
+    public BankApplicationTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class BankApplication.
+     */
+    @Test
+    public void testMain() {
+        assertTrue("It is not true", true);
+    }
+    
+}
